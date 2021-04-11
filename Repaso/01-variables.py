5506391e9f6279445429b53e075d7540a5f620eb
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-04-10 23:52:51 -0500

    Es el Primer Commit, se creo la carpeta Repaso y dentro dos archivos donde se practico la clase del Prof. Eduardo

---
+++ Repaso/01-variables.py
@@ -0,0 +1,23 @@
+#Variables numericas
+#Una variable es un lugar reservado en la memoria
+variable1 = 10
+variable2 = 10.5
+variable3 = False
+#Print es un metodo que se utiliza para imprimir en consola:
+#Shorcut = Ctr + Shif + ñ  ----> es para abrir la consola de ejecución
+# Para linux = Ctrl + Shif + tecla(*+)
+#con la variable "type" es para saber qeu tipo de variable estamos pasando
+print(type(variable1))
+print(type(variable3))
+
+#VARIABLES DE TEXTO
+variable4 = "Eduardo"
+variable5 = 'Arias'
+print(len(variable4))
+
+#Variables NUNCA comienzan con numeros, ni tampoco con simbolos
+#Las variables son sensitivas a mayusculas y minusculas
+varIable = 4
+variable = 8
+print(varIable)
+print(variable)
---
+++ Repaso/02-operadores.py
@@ -0,0 +1,49 @@
+#Operadores Aritmeticos
+#Todos los operadores solamente funcinan en variables numericas
+#typeError es cuando qetamos queriendo hacer algon uy no  se puede con ese tipo de variale
+var1 = 27
+var2 = 5
+
+resultado = var1 + var2
+print("El Resultado es:", resultado)
+
+resultado = var1 - var2 #Resta
+resultado = var1 * var2 #Multiplicacion
+resultado = var1 / var2 #Division
+print("La division es:",resultado)
+resultado = var1 % var2 #Modulo | su resultado solo es hasta la parte entera
+print("El modulo es", resultado)
+resultado = var1 // var2 #Cociente | su resultado solo es hasta la parte entera
+print("El cociente es:",resultado)
+resultado = var1 ** var2 #Exponente
+print("El exponente es:",resultado)
+
+#OPERADORES DE ASIGNACION
+#Igual (=)
+#Incremento
+resultado = resultado + var1
+resultado += var1
+# Decremento
+resultado -= var1
+# Multiplicacion
+resultado *= var1
+# Division
+resultado /= var1
+# Potencia
+resultado **= var1
+
+#Operadores de Comparacion
+# = asignacion
+
+print(5 == 4)
+
+# == comparacion, para saber si el valor de la izq es igual al valor de la derecha (retorna un True o False (bool))
+# con la fuente de vsc
+# != para saber si el valor de la izq es diferente al valor de la derecha
+print(5 != 4)
+
+# < > es menor | es mayor
+print(10 > 5)
+
+# >=, <=, es mayor o igual, es menor o igual
+prin
