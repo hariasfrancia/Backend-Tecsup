7e3ef0e60e0d14b8a4077c35522eeb7b7a18e385
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-04-17 21:35:38 -0500

    cree nueva rama

--- Libreria/ProyectoLibreria/gestion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ListarCategoriaController, CRUDCategoriaController
-
-# !Esta variable se tiene quellamar SI o SI así:
-urlpatterns = [
-    path('categorias', ListarCategoriaController.as_view()),
-    path('categorias/<int:pk>', CRUDCategoriaController.as_view()),
-]
